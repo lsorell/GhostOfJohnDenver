aacf001cb4c45acc9a1297c3983146640c35321f
Lane Sorell <lanesorell@gmail.com>
2020-06-05 12:04:38 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ghost of John Denver
+
+A discord bot that has one purpose and one purpose only. To play "Take Me Home Country Road" by John Denver on an endless loop.
---
+++ bot.js
@@ -0,0 +1,27 @@
+var Discord = require('discord.io');
+var fs = require('fs');
+var auth = require('./auth.json');
+
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+
+var VCID = '419001809852694539';
+var song = 'John Denver - Take Me Home Country Roads.mp3';
+
+bot.on('ready', function(event) {
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
+	
+	bot.joinVoiceChannel(VCID, function(err, events) {
+        if (err) return console.error(err);
+
+        bot.getAudioContext(VCID, function(err, stream) {
+            if (err) return console.error(err);			
+            fs.createReadStream(song).pipe(stream, {end: false});
+            stream.on('done', function() {				
+                fs.createReadStream(song).pipe(stream, {end: false});
+            });
+        });
+    });
+});
